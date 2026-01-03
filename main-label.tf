531bf9d1bff0802f3d3c12062083b5af0df2bda9
iksadnorth <iksadnorth@gmail.com>
2026-01-03 09:58:33 +0900

    클라우드 서버에 taint 설정 추가

--- main-label.tf
+++ main-label.tf
@@ -1,4 +1,4 @@
-# 노드 라벨 추가
+# 노드 라벨 및 Taint 추가
 resource "null_resource" "label_node" {
   depends_on = [null_resource.join_cluster]
 
@@ -7,7 +7,8 @@ resource "null_resource" "label_node" {
       ssh -o StrictHostKeyChecking=no \
         -i "${var.k8s_control_plane_ssh_key}" \
         "${var.k8s_control_plane_ssh_user}@${var.k8s_control_plane_ssh_host}" \
-        "kubectl label nodes ${var.node_name} cloud-server=true --overwrite"
+        "kubectl label nodes ${var.node_name} cloud-server=true --overwrite && \
+         kubectl taint nodes ${var.node_name} cloud-server=true:NoSchedule --overwrite"
     EOT
   }
 }
